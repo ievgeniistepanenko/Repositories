0ded13d56d6fc57b3ac410ae2210268aea9d0da4
Ievgenii Stepanenko <iev@conexio.dk>
2018-11-23 12:13:45 +0100

    Add queryable extension

---
+++ Repositories/Extensions/QueryableExtension.cs
@@ -0,0 +1,16 @@
+﻿using Specification.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repositories.Extensions
+{
+    public static class QueryableExtension
+    {
+        public static IQueryable<T> Where<T>(this IQueryable<T> source, ISpecification<T> spec)
+        {
+            return source.Where(spec.ToExpression());
+        }
+    }
+}
